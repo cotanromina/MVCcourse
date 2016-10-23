13d8ea8e43bbaf2f9affaa88ca918da67da4ab20
Romina Cotan <Romina Cotan>
2016-10-23 19:56:48 +0300

    First commit

---
+++ ClassesTutorial/ClassesTutorial/Mercedes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    public class Mercedes : ICar
+    {
+        // public string Description;
+        // public int Price;
+        //private _description;
+        // private _price;
+        //protected _description;
+        // protected _price;
+        internal _description;
+        internal _price;
+    }
+    public Mercedes()
+    {
+        Description = "E Class";
+        Price = 67000;
+    }
+}
---
+++ ClassesTutorial/ClassesTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    struct EngineStruct { };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mercedes mercedes = new Mercedes();
+            EngineStruct engine = new EngineStruct();
+            ICar car = new Mercedes();
+            //AbstractCar car2 = new Mercedes();
+            Mercedes mercedes2 = new Mercedes();
+            Console.WriteLine("This is my first C# project");
+            Console.WriteLine("Mercedes is "+mercedes2.Description);
+            Console.WriteLine("Mercedes costs"+mercedes2.Price);
+            Console.ReadKey();
+        }
+    }
+}
